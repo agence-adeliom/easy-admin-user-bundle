452a3efde5e94e85debfc17644292bbd8432b0c4
JeanDavidDaviet <jeandaviddaviet@gmail.com>
2026-03-16 20:59:39 +0100

    doctrine dbal 4.x upgrade wip

--- src/EventListener/DoctrineMappingListener.php
+++ src/EventListener/DoctrineMappingListener.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Adeliom\EasyAdminUserBundle\EventListener;
 
 use Doctrine\Bundle\DoctrineBundle\Attribute\AsDoctrineListener;
--- tests/EventListener/DoctrineMappingListenerTest.php
+++ tests/EventListener/DoctrineMappingListenerTest.php
@@ -7,6 +7,7 @@ namespace Adeliom\EasyAdminUserBundle\Tests\EventListener;
 use Adeliom\EasyAdminUserBundle\Entity\ResetPasswordRequest;
 use Adeliom\EasyAdminUserBundle\Entity\User;
 use Adeliom\EasyAdminUserBundle\EventListener\DoctrineMappingListener;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Event\LoadClassMetadataEventArgs;
 use Doctrine\ORM\Mapping\ClassMetadata;
 use PHPUnit\Framework\Attributes\CoversClass;
@@ -17,51 +18,33 @@ final class DoctrineMappingListenerTest extends TestCase
 {
     public function testLoadClassMetadataMapsUserAssociationForResetPasswordClass(): void
     {
-        $metadata = $this->createMock(ClassMetadata::class);
-        $metadata->expects(self::once())
-            ->method('getName')
-            ->willReturn(ResetPasswordRequest::class);
-        $metadata->expects(self::once())
-            ->method('hasAssociation')
-            ->with('user')
-            ->willReturn(false);
-        $metadata->expects(self::once())
-            ->method('mapManyToOne')
-            ->with([
-                'fieldName' => 'user',
-                'targetEntity' => User::class,
-            ]);
-
-        $event = $this->createMock(LoadClassMetadataEventArgs::class);
-        $event->expects(self::once())
-            ->method('getClassMetadata')
-            ->willReturn($metadata);
-
+        $metadata = new ClassMetadata(ResetPasswordRequest::class);
         $listener = new DoctrineMappingListener(User::class, ResetPasswordRequest::class);
+        $event = new LoadClassMetadataEventArgs($metadata, $this->createMock(EntityManagerInterface::class));
 
         $listener->loadClassMetadata($event);
+
+        $mapping = $metadata->getAssociationMapping('user');
+
+        self::assertSame('user', $mapping->fieldName);
+        self::assertSame(User::class, $mapping->targetEntity);
     }
 
     public function testLoadClassMetadataSkipsUnrelatedClassesAndExistingAssociation(): void
     {
-        $metadata = $this->createMock(ClassMetadata::class);
-        $metadata->expects(self::exactly(2))
-            ->method('getName')
-            ->willReturnOnConsecutiveCalls(User::class, ResetPasswordRequest::class);
-        $metadata->expects(self::once())
-            ->method('hasAssociation')
-            ->with('user')
-            ->willReturn(true);
-        $metadata->expects(self::never())->method('mapManyToOne');
-
-        $event = $this->createMock(LoadClassMetadataEventArgs::class);
-        $event->expects(self::exactly(2))
-            ->method('getClassMetadata')
-            ->willReturn($metadata);
-
         $listener = new DoctrineMappingListener(User::class, ResetPasswordRequest::class);
+        $manager = $this->createMock(EntityManagerInterface::class);
+
+        $userMetadata = new ClassMetadata(User::class);
+        $listener->loadClassMetadata(new LoadClassMetadataEventArgs($userMetadata, $manager));
+        self::assertFalse($userMetadata->hasAssociation('user'));
+
+        $resetMetadata = new ClassMetadata(ResetPasswordRequest::class);
+        $event = new LoadClassMetadataEventArgs($resetMetadata, $manager);
 
         $listener->loadClassMetadata($event);
         $listener->loadClassMetadata($event);
+
+        self::assertCount(1, $resetMetadata->getAssociationMappings());
     }
 }
